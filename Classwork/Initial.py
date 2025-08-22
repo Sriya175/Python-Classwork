274cf46d2d9ba702b55d24cb4778577525f7420a
Sreedevi Viswambaran <sreedeviviswambaran@Sreedevis-MacBook-Air.local>
2025-08-21 20:38:59 -0700

    first commit

---
+++ Classwork/Initial.py
@@ -0,0 +1,14 @@
+# print("hello world")
+# print(4)
+# print("Hello \nPython")
+# print("hello",5)
+# print("Welcome to",end="*")
+# x=5
+# y="John"
+# print(x)
+# print(y)
+# name=input("enter your name: ")
+# print("\nHello",name,"\nWelcome to Codingal")
+print("Python keywords are\n")
+import keyword
+print(keyword.kwlist)
